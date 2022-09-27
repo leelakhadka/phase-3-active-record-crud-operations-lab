25568e72f91e661a8dcdd1fa0b3a2845e19c471f
leela khadka <khadkaleela3@gmail.com>
2022-09-26 22:49:17 -0700

    completed tests

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,53 @@
 class Movie < ActiveRecord::Base
+      #Read
+      def self.first_movie
+        self.first
+      end
 
+      def self.last_movie
+        self.last
+      end
+
+      def self.movie_count
+        self.count
+      end
+
+      def self.find_movie_with_id(id)
+        self.find(id)
+      end
+
+      def self.find_movie_with_attributes(title, release_date)
+        self.find_by(title, release_date)
+      end
+
+      def self.find_movies_after_2002
+        self.where("release_date > ?", 2002) 
+      end
+
+      #Create
+      def self.create_with_title(title)
+        movie = Movie.new
+        movie[:title] = title
+        movie.save
+        movie
+      end
+
+      #Update
+      def update_with_attributes(title)
+        self.update(title)
+      end
+
+      def self.update_all_titles(title)
+        Movie.update(title)
+      end
+
+
+      #Delete
+      def self.delete_by_id(id)
+        self.destroy(id)
+      end
+
+      def self.delete_all_movies
+        Movie.destroy_all
+      end
 end
